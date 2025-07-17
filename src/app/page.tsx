1a56147d365a50025e262ae69eeac93d73633487
BHARGAVI PAI <22u1276@students.git.edu>
2025-07-17 08:32:47 +0000

    https://www.bing.com/images/search?view=detailV2&ccid=NNTt6SWq&id=BC4C36

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         className="relative w-full h-[60vh] flex items-center justify-center text-center text-white"
       >
         <Image
-          src="https://cdn.corenexis.com/media?0wqlja&168H&p&b&9zq7.jpg"
+          src="https://wallpaperaccess.com/full/505018.jpg"
           alt="A scenic view of an Indian village"
           fill={true}
           style={{objectFit: "cover", objectPosition: "center"}}
